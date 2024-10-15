2567cc74ec749886b2795bd42e6a643d64b8415d
JimmyToluene <jimmytoluene@gmail.com>
2024-10-15 12:23:29 +0800

    11

--- app/src/main/java/com/example/bitsorderpizza/MainActivity.java
+++ app/src/main/java/com/example/bitsorderpizza/MainActivity.java
@@ -1,25 +1,48 @@
 package com.example.bitsorderpizza;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.app.ActionBar;
+import androidx.core.view.MenuItemCompat;
+
 import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private ShareActionProvider shareActionProvider;
+    private void setShareActionIntent(String text){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+        shareActionProvider.setShareIntent(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.order_tool);
         setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main,menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join me for pizza?");
         return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.action_create_order)   {
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        else{
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
